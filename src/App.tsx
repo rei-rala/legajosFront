c4f8d8cf7675633a1d693ede8f181039abd2f834
rei-rala <ramonirala@outlook.com>
2022-07-26 22:05:27 -0300

    better workflow parser

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,129 @@
-import './styles/globals.css'
-import Layout from './components/layout'
-import { useState } from 'react'
+import "./styles/globals.css";
+import Layout from "./components/layout";
+import { useState } from "react";
 
-import moment from 'moment'
+import moment from "moment";
 
 type WorkflowObject = {
   [key: string | number]: any;
-}
+};
 
 type Workflow = {
-  [key: string]: WorkflowObject
+  [key: string]: WorkflowObject;
+};
+
+function getColumnCodSolicitud(titles: string[]) {
+  let currentCodSolColumnName = import.meta.env.VITE_WF_CODIGO_SOLICITUD_COLUMN.toLowerCase().split("|") ?? ["codigo solicitud", "codigo solicitud"];
+
+  return titles.findIndex((elem) => {
+    let titleLower = elem.toLowerCase()
+    return currentCodSolColumnName.includes(titleLower)
+  });
 }
 
-function getColumnSolicitudExpediente(titles: string[]) {
-  let currentCodExpedienteColumnName: string = import.meta.env.VITE_WF_CODIGO_SOLICITUD_COLUMN ?? "Expediente";
+function getColumnCodExpediente(titles: string[]) {
+  let currentCodExpColumnName = import.meta.env.VITE_WF_CODIGO_EXPEDIENTE_COLUMN.toLowerCase().split("|") ?? ["expediente"];
 
-  return titles.findIndex(element => element === currentCodExpedienteColumnName);
-}
-
-
-function getColumnCodigoExpediente(titles: string[]) {
-  let currentCodExpedienteColumnName: string = import.meta.env.VITE_WF_CODIGO_EXPEDIENTE_COLUMN ?? "Expediente";
-
-  return titles.findIndex(element => element === currentCodExpedienteColumnName);
+  return titles.findIndex((elem) => {
+    let titleLower = elem.toLowerCase()
+    return currentCodExpColumnName.includes(titleLower)
+  });
 }
 
 function parseWfObjectByName(title: string, value: string) {
-  let currentWfColumnDate: string[] = import.meta.env.VITE_WF_DATE_COLUMNS || []
-  let currentWfColumnFloat: string[] = import.meta.env.VITE_WF_FLOAT_COLUMNS || []
-  let parsedValue = value.trim()
+  const titleLower = title.toLowerCase()
 
-  if (parsedValue.length == 0) {
+  let currentWfColumnDate = import.meta.env.VITE_WF_DATE_COLUMNS.toLowerCase().split("|") ?? [];
+  let currentWfColumnFloat = import.meta.env.VITE_WF_FLOAT_COLUMNS.toLowerCase().split("|") ?? [];
+
+  let trimmedValue = value.trim();
+  if (trimmedValue.length == 0) {
     return null;
   }
 
   // basic number
-  if (!isNaN(+value)) {
-    return +value
+  if (!isNaN(+trimmedValue)) {
+    return +trimmedValue;
   }
 
   // float
-  if (currentWfColumnFloat.includes(title) || title.toLowerCase().includes('importe')) {
-    return +value.split("").filter(char => char !== ".").map(char => char === "," ? "." : char).join("");
+  if (currentWfColumnFloat.includes(titleLower) || titleLower.includes("importe")) {
+    return +trimmedValue
+      .split("")
+      .filter((char) => char !== ".")
+      .map((char) => (char === "," ? "." : char))
+      .join("");
   }
 
-  if (title.toLowerCase().includes('fecha') || currentWfColumnDate.includes(title)) {
+  if (titleLower.includes("fecha") || currentWfColumnDate.includes(titleLower)) {
     // parse to date
-    return moment(value, "dd/mm/YYYY").toDate()
+    return moment(trimmedValue, "dd/mm/YYYY").toDate();
   }
 
-  return value;
+  return trimmedValue;
 }
 
-
-async function workflowToArray(workflow: string) {
-  const workflowArray = workflow.split("\n")
+async function parseWfToObject(workflow: string) {
   let arrAux: string[][] = [];
 
-  // La ultima linea copiada desde Excel es una linea vacia
-  // De ahi el tope de la lista (-1)
+  // Arrays of rows
+  const workflowArray = workflow.split("\n");
+
+  // Copying an entire excel table leaves a blank row at the end ("\n")
+  // Therefore, omitting last row by default 
   for (let i = 0; i < workflowArray.length - 1; i++) {
     let line = workflowArray[i].split("\t");
-    line = line.map(item => item.trim())
-    arrAux.push(line)
+    line = line.map((item) => item.trim());
+    arrAux.push(line);
   }
 
-  let lineSize = arrAux[0].length
-  arrAux = arrAux.filter(v => v.length === lineSize)
-
+  let lineSize = arrAux[0].length;
+  arrAux = arrAux.filter((v) => v.length === lineSize);
 
   // TODO: Make more && smaller functions
   let columnTitles = arrAux[0];
-  let codSolIndex = getColumnSolicitudExpediente(columnTitles)
-  let codExpIndex = getColumnCodigoExpediente(columnTitles);
+  let codSolIndex = getColumnCodSolicitud(columnTitles);
+  let codExpIndex = getColumnCodExpediente(columnTitles);
 
-  let workflowObject: any = {}
-  console.log(arrAux)
+  const workflowObject: WorkflowObject = {};
 
-  for (let row of arrAux) {
-    const codigoSolicitud = +row[codSolIndex]
-    if (!codigoSolicitud) {
+  if (-1 in [codExpIndex, codSolIndex]) {
+    return null;
+  }
+
+  // Skipping column titles (row 0)
+  for (let i = 1; i < arrAux.length; i++) {
+    let row = arrAux[i];
+
+    const codigoSolicitud = +row[codSolIndex];
+    if (!codigoSolicitud || isNaN(codigoSolicitud)) {
       continue;
     }
 
-    const codigoExpediente = +row[codExpIndex]
-    if (!codigoExpediente) {
+    const codigoExpediente = +row[codExpIndex];
+    if (!codigoExpediente || isNaN(codigoExpediente)) {
       continue;
     }
 
-    const rowObject: any = {}
+    // make "SOLICITUD" as array of "EXPEDIENTES"
+    if (!(workflowObject[codigoSolicitud])) {
+      workflowObject[codigoSolicitud] = {};
+    }
+
+    if (codigoExpediente in workflowObject[codigoSolicitud]) {
+      continue;
+    }
+
+    const rowObject: any = {};
 
     row.forEach((value, index) => {
       let columnTitle = columnTitles[index];
-      rowObject[columnTitle] = parseWfObjectByName(columnTitle, value)
-    })
+      rowObject[columnTitle] = parseWfObjectByName(columnTitle, value);
+    });
 
-
-    // MAKE "SOLICITUD" as ARRAY OF "EXPEDIENTES"
-    if (!rowObject[codigoSolicitud]) {
-      rowObject[codigoSolicitud] = []
-    }
-    rowObject[codigoSolicitud].push(rowObject)
-
-    workflowObject[codigoSolicitud] = { ...workflowObject[codigoSolicitud], ...rowObject }
+    workflowObject[codigoSolicitud][codigoExpediente] = rowObject;
   }
 
-
-  console.info(workflowObject)
   return workflowObject
 }
 
@@ -127,22 +144,23 @@ function App() {
     Promise.resolve()
       .then(() => setIsLoading(true))
       .then(() => parserValue)
-      .then(workflowToArray)
-      .then(console.warn)
-      .catch((err) => alert(err))
-      .finally(() => setIsLoading(false))
+      .then(parseWfToObject)
+      .then(console.info)
+      .catch(alert)
+      .finally(() => setIsLoading(false));
   }
 
   return (
     <Layout>
-      {isLoading && <div>Loading...</div>}
       <div id="PARSER">
         <h2>Bienvenido al parser</h2>
 
+        {isLoading && <b>CARGANDO...</b>}
         <form onSubmit={handleSubmit}>
 
           <label htmlFor="input">Ingrese debajo el workflow correspondiente</label> <br />
-          <textarea id="input"
+          <textarea
+            id="input"
             cols={100} rows={15}
             placeholder="Expediente, Codigo Solicitud, Estado Expediente, [...]"
             value={parserValue}
@@ -156,4 +174,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_WF_DATE_COLUMNS: string[]
-  readonly VITE_WF_FLOAT_COLUMNS: string[]
+  readonly VITE_WF_DATE_COLUMNS: string
+  readonly VITE_WF_FLOAT_COLUMNS: string
   readonly VITE_WF_CODIGO_EXPEDIENTE_COLUMN: string
   readonly VITE_WF_CODIGO_SOLICITUD_COLUMN: string
 
